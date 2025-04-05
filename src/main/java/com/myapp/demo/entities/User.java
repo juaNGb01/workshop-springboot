54a170e1d18cbcce865c76171daf399605fe596c
juan gabriel <juanbogobenitez@gmail.com>
2025-04-05 17:46:12 -0300

    Service layer, component registration

--- src/main/java/com/myapp/demo/entities/User.java
+++ src/main/java/com/myapp/demo/entities/User.java
@@ -5,6 +5,7 @@ import jakarta.persistence.*;
 import java.io.Serializable;
 import java.util.Objects;
 
+
 @Entity
 @Table(name = "tb_user") // cria tabela com respectivo nome
 public class User implements Serializable {
